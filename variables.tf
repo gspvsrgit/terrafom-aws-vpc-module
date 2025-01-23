83dbfc238a756d48511528efa144827d06042410
gspvsr <gspvsr@yahoo.com>
2025-01-23 13:22:53 -0600

    further develed the module on 23.01.2025

--- variables.tf
+++ variables.tf
@@ -1,28 +1,28 @@
 variable "cidr_block" {
-     
+
 }
 
 variable "enable_dns_hostnames" {
-    default = true
+  default = true
 }
 
 variable "enable_dns_support" {
-    default = true
+  default = true
 }
 
 variable "common_tags" {
-    type = map
-    default = {}
+  type    = map(any)
+  default = {}
 }
 
 variable "vpc_tags" {
-    type = map
-    default = {}
+  type    = map(any)
+  default = {}
 }
 
 variable "igw_tags" {
-    type = map
-    default = {}
+  type    = map(any)
+  default = {}
 }
 
 variable "public_subnet_cidr" {
@@ -34,7 +34,7 @@ variable "public_subnet_names" {
 }
 
 variable "azs" {
-    
+
 }
 
 variable "private_subnet_cidr" {
@@ -51,5 +51,19 @@ variable "database_subnet_cidr" {
 }
 
 variable "database_subnet_names" {
+
+}
+
+variable "public_route_table_tags" {
+  default = {}
+
+}
+
+variable "database_route_table_tags" {
+
+
+}
+variable "private_route_table_tags" {
+
 
 }
